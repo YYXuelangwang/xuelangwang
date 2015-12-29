5d78833757d66308848e90156c10a85f95c0e8b9
shirley <yy_xuelangwang@sina.com>
2015-12-29 15:28:45 +0800

    kong

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by tarena3807 on 15/12/29.
+//  Copyright © 2015年 tarena29. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
